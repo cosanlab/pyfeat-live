1a319a40044a04a9418bf4e9f8d71615050003bc
ejolly <eshin.jolly@gmail.com>
2023-10-03 22:28:33 -0400

    basic image viewer

--- pages/Analyze.py
+++ pages/Analyze.py
@@ -5,11 +5,21 @@ from feat.utils.io import read_feat
 from pathlib import Path
 import pandas as pd
 
-st.set_page_config(layout="wide")
+st.set_page_config()
 
 img_folder = Path("./static/detections")
 fex_file = Path("./static/detections.csv")
 
+if "num_images" not in st.session_state:
+    st.session_state.num_images = 0
+if "img_idx" not in st.session_state:
+    st.session_state.img_idx = 0
+if "live_data" not in st.session_state:
+    st.session_state.live_data = False
+if "upload_data" not in st.session_state:
+    st.session_state.upload_data = False
+if "show_select_container" not in st.session_state:
+    st.session_state.show_select_container = True
 
 def show_data():
     """Render uploaded data or live detection data"""
@@ -36,6 +46,34 @@ def show_data():
         )
 
 
+def increment_idx():
+    st.session_state.img_idx = min(
+        st.session_state.img_idx + 1, st.session_state.num_images - 1
+    )
+    st.session_state.live_data = True
+
+
+def decrement_idx():
+    st.session_state.img_idx = max(st.session_state.img_idx - 1, 0)
+    st.session_state.live_data = True
+
+
+def show_images():
+    imgs = sorted(map(str, img_folder.glob("*.png")))
+    st.session_state.num_images = len(imgs)
+
+    st.image(imgs[st.session_state.img_idx])
+
+    left, center, right = st.columns(3, gap="large")
+    with left:
+        if st.button("Previous"):
+            decrement_idx()
+    with center:
+        st.write(f"**Frame:** {st.session_state.img_idx}")
+    with right:
+        st.button("Next", on_click=increment_idx)
+
+
 @st.cache_data
 def convert_live_data(df):
     return df.to_csv().encode("utf-8")
@@ -44,15 +82,25 @@ def convert_live_data(df):
 # %%
 
 st.write("# Analyze")
-if st.session_state.get("live_data") or st.session_state.get("upload_data"):
-    show_data()
-else:
+if st.session_state.show_select_container:
     st.write(
         "You can analyze a csv file or list of images by uploading a file or trying to load save detections from the live demo"
     )
-    st.file_uploader("Choose a csv file", key="upload_data")
+    upload_data = st.file_uploader("Choose a csv file")
+    if upload_data is not None:
+        st.session_state.upload_data = upload_data
+        st.session_state.show_select_container = False
+        st.rerun()
     if fex_file.exists():
-        st.button("Use live detections", key="live_data")
+        if st.button("Use live detections"):
+            st.session_state.live_data = True
+            st.session_state.show_select_container = False
+elif st.session_state.get("live_data"):
+    show_images()
+    if st.toggle("Display csv"):
+        show_data()
+elif st.session_state.get("upload_data"):
+    show_data()
 
 with st.sidebar:
     if fex_file.exists():
