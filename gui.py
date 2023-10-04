ffb3d84a564763fea5e3d461d8001efc1076d5f0
ejolly <eshin.jolly@gmail.com>
2023-10-03 20:55:35 -0400

    basic gui scaffold with pywebview

---
+++ gui.py
@@ -0,0 +1,51 @@
+import webview
+import os
+from subprocess import Popen, PIPE, STDOUT
+import sys
+import time
+
+
+def main():
+
+    # Getting path to python executable (full path of deployed python on Windows)
+    executable = sys.executable
+
+    path_to_main = os.path.join(os.path.dirname(__file__), "Detect.py")
+
+    # Running streamlit server in a subprocess and writing to log file
+    proc = Popen(
+        [
+            executable,
+            "-m",
+            "streamlit",
+            "run",
+            path_to_main,
+            # The following option appears to be necessary to correctly start the streamlit server,
+            # but it should start without it. More investigations should be carried out.
+            "--server.headless=true",
+            "--global.developmentMode=false",
+        ],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=STDOUT,
+        text=True,
+    )
+    proc.stdin.close()
+
+    # Force the opening (does not open automatically) of the browser tab after a brief delay to let
+    # the streamlit server start.
+    time.sleep(3)
+    window = webview.create_window("Py-Feat Live", 'tp://localhost:8501')
+    webview.start()
+
+    while True:
+        s = proc.stdout.read()
+        if not s:
+            break
+        print(s, end="")
+
+    proc.wait()
+
+
+if __name__ == "__main__":
+    main()
