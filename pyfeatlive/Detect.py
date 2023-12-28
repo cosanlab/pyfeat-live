c081d78f2f3b80b37db5bf824f10e5eac5d5952d
ljchang <luke.j.chang@dartmouth.edu>
2023-12-28 18:56:23 -0500

    pyfeatlive with recording in memory

--- pyfeatlive/Detect.py
+++ pyfeatlive/Detect.py
@@ -9,34 +9,23 @@
 import queue
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode
-from aiortc.contrib.media import MediaRecorder
+
+# from aiortc.contrib.media import MediaRecorder
 from feat import Detector
 import time
 import plotly.graph_objects as go
 from utils import process_frame, make_plotly_fig
 from pathlib import Path
-import shutil
 import logging
-import tempfile
 import pandas as pd
-from io import StringIO
-import numpy as np
+import av
+from io import BytesIO
 
 webrtc_logger = logging.getLogger("streamlit_webrtc")
 webrtc_logger.setLevel(logging.ERROR)
 
 # Video and plotting dimensions
 WIDTH, HEIGHT = 640, 480
-
-# record_folder = tempfile.mkdtemp()
-
-record_folder = Path("./recordings")
-record_folder.mkdir(exist_ok=True)
-
-fex_file = record_folder.joinpath(Path("detections.csv"))
-in_file = record_folder.joinpath(Path(f"pyfeatlive_recording.mp4"))
-# with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp_file:
-#     in_file = Path(tmp_file.name)
 
 # Initialized detectors
 if "face_model" not in st.session_state:
@@ -49,6 +38,14 @@ if "au_model" not in st.session_state:
     st.session_state.au_model = "xgb"
 if "emotion_model" not in st.session_state:
     st.session_state.emotion_model = "resmasknet"
+if "combined_fex" not in st.session_state:
+    st.session_state.combined_fex = []
+if "combined_frames" not in st.session_state:
+    st.session_state.combined_frames = []
+if "avg_fps" not in st.session_state:
+    st.session_state.avg_fps = 0
+if "start_time" not in st.session_state:
+    st.session_state.start_time = time.strftime("%Y%m%d-%H%M%S")
 
 st.set_page_config(layout="wide")
 
@@ -67,16 +64,6 @@ def load_detector():
     )
 
 
-def clear_fex_file():
-    if fex_file.exists():
-        fex_file.unlink()
-
-
-def clear_imgs():
-    # shutil.rmtree(Path(record_folder))
-    shutil.rmtree(record_folder)
-
-
 # %% MAIN UI CODE
 def app():
     # Load detectors
@@ -91,10 +78,43 @@ def app():
             emotion_model=st.session_state.emotion_model,
         )
 
-    def in_recorder_factory() -> MediaRecorder:
-        return MediaRecorder(
-            str(in_file), format="mp4"
-        )  # HLS does not work. See https://github.com/aiortc/aiortc/issues/331
+    def fex_to_csv(fex_data, file_name=None):
+        fex_df = pd.concat(fex_data, axis=0)
+        fex_df["input"] = file_name
+        csv_string = fex_df.to_csv(index=False)
+        return csv_string.encode("utf-8")
+
+    def frames_to_video_in_memory(frames, fps=20, format="mp4"):
+        # Create an in-memory bytes buffer
+        buffer = BytesIO()
+
+        # Open an output container in memory, specifying the format (e.g., 'mp4')
+        output = av.open(buffer, "w", format=format)
+
+        # Add a video stream to the container
+        video_stream = output.add_stream("mpeg4", rate=fps)
+        video_stream.width = frames[0].width
+        video_stream.height = frames[0].height
+        video_stream.pix_fmt = frames[0].format.name
+
+        pts = frames[0].time_base.denominator
+        time_base = 1.0 / fps
+        for frame in frames:
+            frame.pts = pts
+            # pts += int(time_base * fps)
+            pts += int((1 / fps) * frame.time_base.denominator)
+            # Convert PyAV frame to a packet and write to the container
+            for packet in video_stream.encode(frame):
+                output.mux(packet)
+
+        # Finalize and close the container
+        for packet in video_stream.encode():
+            output.mux(packet)
+        output.close()
+
+        # Go to the beginning of the buffer
+        buffer.seek(0)
+        return buffer
 
     # Initialize
     frame_counter = 0
@@ -114,28 +134,35 @@ def app():
     with st.sidebar:
         st.write("### Saving detections")
         st.checkbox("Record Session", key="save_session", value=True)
-        start_time = time.strftime("%Y%m%d-%H%M%S")
-        # st.checkbox("Save detections", key="save_fex", value=False)
-        # st.checkbox("Save images", key="save_img", value=False)
-        if fex_file.exists():
-            with fex_file.open("rb") as f:
-                st.download_button(
-                    label="Download Detections",
-                    data=f,
-                    file_name=f"pyfeatlive_detections_{start_time}.csv",
-                )
-            # st.button("Clear detections", key="delete_fex", on_click=clear_fex_file)
-        # if any(Path(record_folder).iterdir()):
-        # if any(record_folder.iterdir()):
-        #     st.button("Clear images", key="delete_img", on_click=clear_imgs)
-        if in_file.exists():
-            with in_file.open("rb") as f:
-                st.download_button(
-                    label="Download Video",
-                    data=f,
-                    file_name=f"pyfeatlive_recording_{start_time}.mp4",
-                )
 
+        # Check if there is fex data to download
+        if st.session_state.save_session and st.session_state.combined_fex:
+            # Only create the download button if there is data
+            st.download_button(
+                label="Download Fex",
+                data=fex_to_csv(
+                    st.session_state.combined_fex,
+                    file_name=f"pyfeatlive_video_{st.session_state.start_time}.csv",
+                ),
+                file_name=f"pyfeatlive_fex_{st.session_state.start_time}.csv",
+                mime="text/csv",
+            )
+        else:
+            st.write("Fex unavailable to download.")
+
+        # Check if there is video data to download
+        if st.session_state.save_session and st.session_state.combined_frames:
+            st.download_button(
+                label="Download Video",
+                data=frames_to_video_in_memory(
+                    st.session_state.combined_frames,
+                    fps=int(1 / st.session_state.avg_fps),
+                ),
+                file_name=f"pyfeatlive_video_{st.session_state.start_time}.mp4",
+                mime="video/mp4",
+            )
+        else:
+            st.write("Video unavailable to download.")
         st.divider()
 
         st.write("### Swap detectors")
@@ -180,57 +207,20 @@ def app():
     fps = st.empty()
 
     # Create WebRTC cam
-    if st.session_state.save_session:
-        ctx = webrtc_streamer(
-            key="sample",
-            mode=WebRtcMode.SENDONLY,
-            media_stream_constraints={
-                "video": {"width": WIDTH, "height": HEIGHT},
-                "audio": False,
-            },
-            async_processing=True,
-            in_recorder_factory=in_recorder_factory,
-        )
-    else:
-        ctx = webrtc_streamer(
-            key="sample",
-            mode=WebRtcMode.SENDONLY,
-            media_stream_constraints={
-                "video": {"width": WIDTH, "height": HEIGHT},
-                "audio": False,
-            },
-            async_processing=True,
-        )
+    ctx = webrtc_streamer(
+        key="sample",
+        mode=WebRtcMode.SENDONLY,
+        media_stream_constraints={
+            "video": {"width": WIDTH, "height": HEIGHT},
+            "audio": False,
+        },
+        async_processing=True,
+    )
     # Each button is has two-way binding it's key kwarg in st.session_state.key
     # st.session_state can then be used to read values within functions above to
     # do selecting processing/rendering without complicated threads and queues
     # Create button row
     col1, col2, col3, col4, col5 = st.columns(5)
-    # timestamp = time.strftime("%Y%m%d-%H%M%S")
-    # with col00:
-    #     if in_file.exists():
-    #         with in_file.open("rb") as f:
-    #             st.download_button(
-    #                 label="Download Video",
-    #                 data=f,
-    #                 file_name=f"pyfeatlive_recording_{timestamp}.mp4",
-    #             )
-    # with col01:
-    #     if fex_combined:
-    #         fex_readout = pd.concat(fex_combined, axis=1)
-    #     else:
-    #         fex_readout = pd.DataFrame(np.random.random((10, 10)))
-    #     # csv_buffer = StringIO()
-    #     csv_string = fex_readout.to_csv(index=False)
-    #     csv_bytes = csv_string.encode("utf-8")
-    #     # csv_buffer.seek(0)
-    #     st.download_button(
-    #         label="Download Fex",
-    #         data=csv_bytes,
-    #         file_name=f"pyfeatlive_fex_{timestamp}.csv",
-    #         mime="text/csv",
-    #     )
-
     with col1:
         st.checkbox("Facebox", key="rects", value=True)
     with col2:
@@ -273,46 +263,21 @@ def app():
 
                 # Update FPS counter
                 now = time.perf_counter()
-                fps.text(f"FPS: {1 / (now-start):.3f}\nIFI: {(now-start):.3f}ms")
+                current_fps = now - start
+                st.session_state.avg_fps += current_fps
+                st.session_state.avg_fps /= 2
+
+                fps.text(f"FPS: {1 / current_fps:.3f}\nIFI: {current_fps:.3f}ms")
                 start = now
 
                 # Make figure
                 make_plotly_fig(figure, fex, img)
                 plot.plotly_chart(figure, use_container_width=True)
 
-                # Update Combined Fex
-                # fex_combined.append(fex)
-                # # print(pd.concat(fex_combined, axis=1))
-                # with col0:
-                #     try:
-                #         fex_readout = pd.concat(fex_combined, axis=1)
-                #         st.download_button(
-                #             label="Download Detections",
-                #             data=fex_readout,
-                #             file_name=fex_file,
-                #             mime="text/csv",
-                #         )
-                #     except:
-                #         pass
-                # print(frame_counter)
-
-                if st.session_state.save_session and not fex.empty:
-                    fex["frame"] = frame_counter
-                    if fex_file.exists():
-                        fex.to_csv(fex_file, mode="a", header=False, index=False)
-                    else:
-                        fex.to_csv(fex_file, index=False)
-
-                # if st.session_state.save_fex and not fex.empty:
-                #     fex["frame"] = current_time
-                #     if fex_file.exists():
-                #         fex.to_csv(fex_file, mode="a", header=False, index=False)
-                #     else:
-                #         fex.to_csv(fex_file, index=False)
-
-                # if st.session_state.save_img:
-                #     figure.write_image(record_folder / f"{current_time}.png")
-                #     # figure.write_image(Path(record_folder) / f"{current_time}.png")
+                # Update Save Frames
+                if st.session_state.save_session:
+                    st.session_state.combined_frames.append(frame)
+                    st.session_state.combined_fex.append(fex)
 
             except queue.Empty:
                 break
@@ -320,98 +285,3 @@ def app():
 
 if __name__ == "__main__":
     app()
-
-# %%
-
-# # Initialize Buttons
-# video_button_enabled = False
-# fex_button_enabled = False
-
-# with tempfile.TemporaryDirectory() as tmp_dir:
-#     record_folder = Path(tmp_dir)
-#     fex_file = record_folder.joinpath(Path("detections.csv"))
-#     in_file = record_folder.joinpath(f"pyfeatlive_recording.mp4")
-
-# File saving  - switch to temporary file
-# record_folder = Path("./recordings")
-# record_folder.mkdir(exist_ok=True)
-# img_folder = Path("./static/detections")
-# img_folder.mkdir(exist_ok=True)
-# if not img_folder.exists():
-#     img_folder.mkdir()
-# print("Temporary directory:", tmp_dir)
-# Initialize prefix for saving video
-# if "prefix" not in st.session_state:
-#     st.session_state["prefix"] = str(uuid.uuid4())
-# prefix = st.session_state["prefix"]
-
-# @st.cache_data
-# def convert_fex():
-#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-#     if fex in globals():
-#         return df.to_csv().encode("utf-8")
-
-# def download_video():
-#     timestamp = time.strftime("%Y%m%d-%H%M%S")
-#     try:
-#         if in_file.exists():
-#             with in_file.open("rb") as f:
-#                 st.download_button(
-#                     label="Download the recorded video",
-#                     data=f,
-#                     file_name=f"pyfeatlive_recording_{timestamp}.mp4",
-#                 )
-#     except:
-#         pass
-
-# st.button("Download video", key="download_video", on_click=download_video)
-# if video_button_enabled:
-#     if st.button(
-#         "Download video", key="download_video", on_click=download_video
-#     ):
-#         st.write("Video Downloaded")
-#     else:
-#         st.write("Video Unavailable")
-
-# video_button_enabled = True
-
-# csv = convert_df(fex)
-# st.download_button(
-#     label="Download Detections",
-#     data=convert_fex(),
-#     file_name=fex_file,
-#     mime="text/csv",
-# )
-
-# # File saving
-# fex_file = Path("./static/detections.csv")
-# img_folder = Path("./static/detections")
-# if not img_folder.exists():
-#     img_folder.mkdir()
-
-
-# # This works, but isn't ideal.
-# timestamp = time.strftime("%Y%m%d-%H%M%S")
-#    with col00:
-#         if in_file.exists():
-#             with in_file.open("rb") as f:
-#                 st.download_button(
-#                     label="Download Video",
-#                     data=f,
-#                     file_name=f"pyfeatlive_recording_{timestamp}.mp4",
-#                 )
-#     with col01:
-#         if fex_combined:
-#             fex_readout = pd.concat(fex_combined, axis=1)
-#         else:
-#             fex_readout = pd.DataFrame(np.random.random((10, 10)))
-#         # csv_buffer = StringIO()
-#         csv_string = fex_readout.to_csv(index=False)
-#         csv_bytes = csv_string.encode("utf-8")
-#         # csv_buffer.seek(0)
-#         st.download_button(
-#             label="Download Fex",
-#             data=csv_bytes,
-#             file_name=f"pyfeatlive_fex_{timestamp}.csv",
-#             mime="text/csv",
-#         )
