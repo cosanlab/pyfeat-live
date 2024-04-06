78885c192565b31baeb62afb630cb214940717a4
ejolly <eshin.jolly@gmail.com>
2024-04-05 20:03:34 -0400

    add logo file

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version="1.0",
     install_requires=requirements,
     packages=["pyfeatlive"],
+    package_data={"pyfeatlive": ["*.png"]},
     entry_points={
         "console_scripts": [
             "pyfeat-live=pyfeatlive.Detect_entry_point:main",
