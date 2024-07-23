c8fe59ce991674a6223bec73a1280ef0dad88d2d
ejolly <eshin.jolly@gmail.com>
2024-07-23 17:42:39 -0400

    hide UI while processing

--- pyfeatlive/analyze.py
+++ pyfeatlive/analyze.py
@@ -75,90 +75,163 @@ if st.session_state.show_select_container:
 
 # Analysis UI
 if st.session_state.show_analyze_ui:
+
     # File upload viewer
     if st.session_state.upload_filetype == "video":
         st.video(st.session_state.upload_data)
 
+    # TODO: image gallery or single image viewer
     elif st.session_state.upload_filetype == "image":
         st.image(st.session_state.upload_data)
 
-    # Detection UI
-    # st.write("## Detector Options")
+    status = st.status("## Detector Options", expanded=True, state="complete")
 
-    with st.status("## Detector Options", expanded=True, state="complete") as status:
+    run_detection = status.button(
+        "Process File",
+        type="primary",
+        on_click=lambda: status.update(
+            label="Processing", state="running", expanded=False
+        ),
+    )
+    status.info("*Optional: Adjust detection settings below*")
+    status.info(
+        "*For longer videos you may want to increase how many frames you skip to speed things up (e.g. process every 24, 30, or 60 frames depending on your captured FPS).*"
+    )
 
-        run_detection = st.button(
-            "Process File",
-            type="primary",
-            on_click=lambda: status.update(
-                label="Processing", state="running", expanded=False
-            ),
+    basic_tab, advanced_tab = status.tabs(["Basic Settings", "Advanced Options"])
+
+    # Main video options
+    with basic_tab:
+        st.write("### Basic Settings")
+        face_detection_threshold = st.slider(
+            "Face Detection Threshold",
+            min_value=0.01,
+            max_value=1.0,
+            value=0.5,
+            help="Confidence of the face detector. Increase if you're getting false or multiple detections and decrease if you're missing faces.",
+        )
+        face_identity_threshold = st.slider(
+            "Face Identity Threshold",
+            min_value=0.01,
+            max_value=1.0,
+            value=0.8,
+            help="Similarity threshold for what embeddings count as the same identity/person",
+        )
+        batch_size = st.number_input(
+            "Batch Size",
+            value=1,
+            help="How many frames you want to bundle in a batch to speed up processed on GPU. Larger values give faster processing at the cost of more memory",
+        )
+        skip_frames = st.number_input(
+            "Number of frames to skip",
+            value=None,
+            help="Only process every Nth frame to speed up detection. Leave blank to process all frames (warning: could take a while to process!)",
         )
 
-        st.info("*Optional: Adjust detection settings below*")
-        st.info(
-            "*For longer videos you may want to increase how many frames you skip to speed things up (e.g. process every 24, 30, or 60 frames depending on your captured FPS).*"
+    # Advanced options
+    with advanced_tab:
+        st.header("Advanced Options")
+        output_size = st.number_input(
+            "Output Size",
+            value=700,
+            help="Image size to rescale all frames while preserving aspect ratio",
+        )
+        num_workers = st.number_input(
+            "Number of Workers",
+            value=0,
+            help="How many subprocesses to user for data loading. 0 means data will be loaded into the main process",
+        )
+        pin_memory = st.checkbox(
+            "Pin Memory",
+            value=False,
+            help="If True, the data loader will copy Tensors into CUDA pinned memory before returning them. If your data elements are a custom type, or your collate_fn returns a batch that is a custom type",
         )
 
-        basic_tab, advanced_tab = st.tabs(["Basic Settings", "Advanced Options"])
+    if run_detection:
+        status.update(label="## Processing", expanded=False, state="running")
+        # Create a temporary filepath to pass to py-feat
+        with NamedTemporaryFile(suffix=".mp4") as temp:
+            temp.write(st.session_state.upload_file.getvalue())
+            temp.seek(0)
+            output = st.session_state.detector.detect_video(temp.name)
+            status.update(label="## Complete", expanded=False, state="complete")
 
-        # Main video options
-        with basic_tab:
-            st.write("### Basic Settings")
-            face_detection_threshold = st.slider(
-                "Face Detection Threshold",
-                min_value=0.01,
-                max_value=1.0,
-                value=0.5,
-                help="Confidence of the face detector. Increase if you're getting false or multiple detections and decrease if you're missing faces.",
-            )
-            face_identity_threshold = st.slider(
-                "Face Identity Threshold",
-                min_value=0.01,
-                max_value=1.0,
-                value=0.8,
-                help="Similarity threshold for what embeddings count as the same identity/person",
-            )
-            batch_size = st.number_input(
-                "Batch Size",
-                value=1,
-                help="How many frames you want to bundle in a batch to speed up processed on GPU. Larger values give faster processing at the cost of more memory",
-            )
-            skip_frames = st.number_input(
-                "Number of frames to skip",
-                value=None,
-                help="Only process every Nth frame to speed up detection. Leave blank to process all frames (warning: could take a while to process!)",
-            )
+        st.dataframe(output)
 
-        # Advanced options
-        with advanced_tab:
-            st.header("Advanced Options")
-            output_size = st.number_input(
-                "Output Size",
-                value=700,
-                help="Image size to rescale all frames while preserving aspect ratio",
-            )
-            num_workers = st.number_input(
-                "Number of Workers",
-                value=0,
-                help="How many subprocesses to user for data loading. 0 means data will be loaded into the main process",
-            )
-            pin_memory = st.checkbox(
-                "Pin Memory",
-                value=False,
-                help="If True, the data loader will copy Tensors into CUDA pinned memory before returning them. If your data elements are a custom type, or your collate_fn returns a batch that is a custom type",
-            )
+    # with st.status("## Detector Options", expanded=True, state="complete") as status:
 
-        if run_detection:
-            output = None
-            while output is None:
-                # Create a temporary filepath to pass to py-feat
-                with NamedTemporaryFile(suffix=".mp4") as temp:
-                    temp.write(st.session_state.upload_file.getvalue())
-                    temp.seek(0)
-                    output = st.session_state.detector.detect_video(temp.name)
-                # fex -> pandas
-            st.dataframe(output)
+    #     run_detection = st.button(
+    #         "Process File",
+    #         type="primary",
+    #         on_click=lambda: status.update(
+    #             label="Processing", state="running", expanded=False
+    #         ),
+    #     )
+
+    #     st.info("*Optional: Adjust detection settings below*")
+    #     st.info(
+    #         "*For longer videos you may want to increase how many frames you skip to speed things up (e.g. process every 24, 30, or 60 frames depending on your captured FPS).*"
+    #     )
+
+    #     basic_tab, advanced_tab = st.tabs(["Basic Settings", "Advanced Options"])
+
+    #     # Main video options
+    #     with basic_tab:
+    #         st.write("### Basic Settings")
+    #         face_detection_threshold = st.slider(
+    #             "Face Detection Threshold",
+    #             min_value=0.01,
+    #             max_value=1.0,
+    #             value=0.5,
+    #             help="Confidence of the face detector. Increase if you're getting false or multiple detections and decrease if you're missing faces.",
+    #         )
+    #         face_identity_threshold = st.slider(
+    #             "Face Identity Threshold",
+    #             min_value=0.01,
+    #             max_value=1.0,
+    #             value=0.8,
+    #             help="Similarity threshold for what embeddings count as the same identity/person",
+    #         )
+    #         batch_size = st.number_input(
+    #             "Batch Size",
+    #             value=1,
+    #             help="How many frames you want to bundle in a batch to speed up processed on GPU. Larger values give faster processing at the cost of more memory",
+    #         )
+    #         skip_frames = st.number_input(
+    #             "Number of frames to skip",
+    #             value=None,
+    #             help="Only process every Nth frame to speed up detection. Leave blank to process all frames (warning: could take a while to process!)",
+    #         )
+
+    #     # Advanced options
+    #     with advanced_tab:
+    #         st.header("Advanced Options")
+    #         output_size = st.number_input(
+    #             "Output Size",
+    #             value=700,
+    #             help="Image size to rescale all frames while preserving aspect ratio",
+    #         )
+    #         num_workers = st.number_input(
+    #             "Number of Workers",
+    #             value=0,
+    #             help="How many subprocesses to user for data loading. 0 means data will be loaded into the main process",
+    #         )
+    #         pin_memory = st.checkbox(
+    #             "Pin Memory",
+    #             value=False,
+    #             help="If True, the data loader will copy Tensors into CUDA pinned memory before returning them. If your data elements are a custom type, or your collate_fn returns a batch that is a custom type",
+    #         )
+
+    # if run_detection:
+    #     output = None
+    #     while output is None:
+    #         # Create a temporary filepath to pass to py-feat
+    #         with NamedTemporaryFile(suffix=".mp4") as temp:
+    #             temp.write(st.session_state.upload_file.getvalue())
+    #             temp.seek(0)
+    #             output = st.session_state.detector.detect_video(temp.name)
+    #         # fex -> pandas
+    #     st.dataframe(output)
 
     # Video
 
